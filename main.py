aeeb1759ed19bdedbbdbf9a09af20b5af95e22e7
Cat Luong <luongcat162002@gmail.com>
2023-10-08 11:49:37 +0000

    Make modification to use in application interface

--- main.py
+++ main.py
@@ -85,52 +85,6 @@ def load_pdf_to_text(pdf_path: str):
 
     return text
 
-def rag_process(pdf_path: str, text: str, model_name: str, question: str):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 200,
-        chunk_overlap = 40,
-        length_function = len
-    )
-    chunks = text_splitter.split_text(text=text)
-
-    store_name = pdf_path[:-4]
-        
-    if os.path.exists(f"{store_name}.pkl"):
-        with open(f"{store_name}.pkl","rb") as f:
-            vectorstore = pickle.load(f)
-        #st.write("Already, Embeddings loaded from the your folder (disks)")
-    else:
-        #embedding (Openai methods) 
-        embeddings = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
-
-        #Store the chunks part in db (vector)
-        vectorstore = FAISS.from_texts(chunks,embedding=embeddings)
-
-        with open(f"{store_name}.pkl","wb") as f:
-            pickle.dump(vectorstore,f)
-
-    retriever = vectorstore.as_retriever()
-    prompt_template = "### Instruction: Use the following context to answer the question. Base your answers on the context given, do not make up information. If no question is given, wait for a question to be asked.\nContext:\n{context}\nQuestion: {question}\n### Response: \n"
-    template = PromptTemplate(
-        template=prompt_template,
-        input_variables=["context", "question"],
-    )
-
-    llm = HuggingFacePipeline.from_model_id(
-        model_id=model_name,
-        task="text-generation",
-        device=device,  # -1 for CPU
-        batch_size=1,  # adjust as needed based on GPU map and model size.
-        model_kwargs={"do_sample": True, "temperature": 0.8, "max_length": 2048, "torch_dtype": torch.bfloat16, "trust_remote_code": True},
-    )
-    chain_type_kwargs = {"prompt": template}
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
-    
-    # question = input("Input Question: ")
-    response = chain.run(question)
-
-    return response
-
 def init_chain(model_name: str, key: Optional[str] = None) -> RetrievalQA:
     pdf_path = "test.pdf"
     text = load_pdf_to_text(pdf_path)
@@ -179,6 +133,10 @@ def init_chain(model_name: str, key: Optional[str] = None) -> RetrievalQA:
     chain_type_kwargs = {"prompt": template}
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
     return chain
+
+def generate_response(question: str, chain: RetrievalQA):
+    response = chain.run(question)
+    return response
 
 def main(chain: RetrievalQA): 
     question = input("Input Question: ")
